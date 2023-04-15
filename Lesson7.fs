bac89071d6a106b746335fdad81766606be3c6de
Sergei <sk_developer@mail.ru>
2023-04-15 11:08:53 +0300

    Lesson7

---
+++ Lesson7.fs
@@ -0,0 +1,18 @@
+// 7.1.1
+let rec fibo = function
+    | 0 -> 0
+    | 1 -> 1
+    | n -> fibo (n-1) + fibo (n-2)
+
+// 7.1.2
+let rec sum = function
+    | 1 -> 1
+    | n -> n + sum (n-1)
+// printfn "%d" (sum 3) 
+
+// 7.1.3
+let rec sum2 = function 
+ | (m,0) -> m 
+ | (m,n) -> m + n + sum2 (m, n-1)
+
+printfn "%d" (sum2 (2,2))
