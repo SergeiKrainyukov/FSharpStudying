d41f526152dd2c7b1572524003c8138f5a4ba85a
Sergei <sk_developer@mail.ru>
2023-04-29 00:01:10 +0300

    Lesson21

---
+++ Lesson21.fs
@@ -0,0 +1,3 @@
+let curry f x y = f (x, y)
+
+let uncurry f (x, y) = f x y
--- Program.fs
+++ Program.fs
@@ -1,4 +1,5 @@
-﻿// 4.3.1
-printfn "Hello, World!!!"
-// 4.3.2
-printfn "%f"(2.1 + 3.2 * (8.0 / 4.0 + 1.3))
+﻿module SomeNamespace.SomeModule'
+
+let curry f = 0
+
+// let uncurry f = ...
