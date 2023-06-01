bf0753db0adf29601c4761e2927a99e7fa80d539
Sergei <sk_developer@mail.ru>
2023-06-01 12:27:52 +0300

    T51

--- Program.fs
+++ Program.fs
@@ -429,3 +429,48 @@ let seq_seq = seqSeq 0
 seq_seq
 |> Seq.take 10
 |> Seq.iter (printfn "%A")
+
+
+// type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
+
+// let rec nat (n:int) : 'a cell = Cons (n, lazy(nat(n+1)))
+
+// let n0 = nat 0
+
+// let hd (s : 'a cell) : 'a =
+//   match s with
+//     Nil -> failwith "hd"
+//   | Cons (x, _) -> x
+
+
+
+
+
+type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
+
+let rec nat (n:int) : 'a cell = Cons (n, lazy(nat(n+1)))
+
+let n0 = nat 5
+
+let hd (s : 'a cell) : 'a =
+  match s with
+    Nil -> failwith "hd"
+  | Cons (x, _) -> x
+
+let tl (s : 'a cell) : Lazy<'a cell> =
+  match s with
+    Nil -> failwith "tl"
+  | Cons (_, g) -> g
+
+
+// 51.3
+let rec nth (s : 'a cell) (n : int) : 'a = 
+   match n with
+   | n when n = 0 -> hd s
+   |_ -> nth ((tl s).Force())(n-1)
+
+printfn "%d" (nth n0 10)
+// printfn "%d" (hd n0)
+
+// например, получить 30000-й элемент:
+// nth n0 30000
